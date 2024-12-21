1b0424161ca9069c72f4c7c6dc57a19eed544b9c
fozery <tagievd31@gmail.com>
2024-12-22 02:07:27 +0300

    feat: add timer in response

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,3 +1,4 @@
+from time import perf_counter
 from typing import List
 from fastapi import APIRouter
 from pydantic import BaseModel, TypeAdapter
@@ -7,17 +8,26 @@ from ..ai.model import predict_priority
 router = APIRouter()
 
 
-class FilteredDataResponse(BaseModel):
-    filteredData: list[ProviderDTO]
+class FilteredResponseDTO(BaseModel):
+    filteredData: List[ProviderDTO]
+    executionTime: float
 
 
-@router.post("/ai_filtered_data")
+@router.post("/ai_filtered_data",  response_model=FilteredResponseDTO)
 async def ai_filtered_data(providers: list[ProviderDTO]):
     # Фильтрация данных с помощью нейронной сети
     json_data = [provider.model_dump() for provider in providers]
 
+    start_time = perf_counter()
     filtered_providers = predict_priority(json_data)
+    end_time = perf_counter()
+
+    execution_time = (end_time - start_time) * 1000
+
     filtered_providers_list = TypeAdapter(
         List[ProviderDTO]).validate_python(filtered_providers)
 
-    return {"filteredData": filtered_providers_list}
+    return FilteredResponseDTO(
+        filteredData=filtered_providers_list,
+        executionTime=execution_time
+    )
